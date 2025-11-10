58a93db52359a94afcbdf265038d01c525ce4862
MonikaRajasekaran <monikarajasekaran16395@gmail.com>
2025-11-10 23:22:20 +0530

    project update

--- src/components/websites.js
+++ src/components/websites.js
@@ -23,41 +23,41 @@ export default function Websites() {
           <div class="portfolio-highlighter w-full md:w-full h-[2px] hidden md:block"></div>
         </div>
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-12 justify-between transition duration-300 mx-4 mb-8 ">
-        <div class="flex flex-col items-center ">
-        <img class="h-64 max-w-xs rounded-lg bg-white bg-cover p-6 shadow-lg" src={Udemy} alt="udemy clone" />
-        <a href="https://monikarajasekaran.github.io/Udemy-clone/" target="_blank" rel="noopener noreferrer">
-      <p class="items-center font-bold p-2 portfolio-highlighter">Visit Website</p>
-    </a>         
-     </div>
-     <div class="flex flex-col items-center ">
-     <img class="h-64 max-w-xs rounded-lg bg-white bg-cover p-6 shadow-lg"  src={Weather} alt="Weather App" />
-  <a href="https://actodo-three.vercel.app/" target="_blank" rel="noopener noreferrer">
-      <p class="items-center font-bold p-2 portfolio-highlighter">Visit Website</p>
-    </a>            </div>
-    <div class="flex flex-col items-center ">
-    <img class="h-64 max-w-xs rounded-lg bg-white bg-cover p-6 shadow-lg"  src={Nostra} alt="Nostra Ecommerce" />
-  <a href="https://monikarajasekaran.github.io/Nosta-Ecommerce/" target="_blank" rel="noopener noreferrer">
-      <p class="items-center font-bold p-2 portfolio-highlighter">Visit Website</p>
-    </a>            </div>
-    <div class="flex flex-col items-center ">
-    <img class="h-64 max-w-xs rounded-lg bg-white bg-cover p-6 shadow-lg"  src={Greenden} alt="Greenden" />
-  <a href="https://monikarajasekaran.github.io/Greenden-Tailwindcss/" target="_blank" rel="noopener noreferrer">
-      <p class="items-center font-bold p-2 portfolio-highlighter">Visit Website</p>
-    </a>            </div>
-    <div class="flex flex-col items-center ">
-    <img class="h-64 max-w-xs rounded-lg bg-white bg-cover p-6 shadow-lg"  src={Triadvisor} alt="Triadvisor" />
-  <a href="https://monikarajasekaran.github.io/Tripadvisor-clone/" target="_blank" rel="noopener noreferrer">
-      <p class="items-center font-bold p-2 portfolio-highlighter">Visit Website</p>
-    </a>            </div>
-    <div class="flex flex-col items-center ">
-            <img class="h-64 max-w-xs rounded-lg bg-white bg-cover p-6 shadow-lg"  src={signup} alt="" />
-  <a href="https://actodo-three.vercel.app/" target="_blank" rel="noopener noreferrer">
-      <p class="items-center font-bold p-2 portfolio-highlighter">Visit Website</p>
-    </a>            </div>
+          <div class="flex flex-col items-center ">
+            <img class="h-64 max-w-xs rounded-lg bg-white bg-cover p-6 shadow-lg" src={Udemy} alt="udemy clone" />
+            <a href="https://monikarajasekaran.github.io/Udemy-clone/" target="_blank" rel="noopener noreferrer">
+              <p class="items-center font-bold p-2 portfolio-highlighter">Visit Website</p>
+            </a>
+          </div>
+          {/* <div class="flex flex-col items-center ">
+            <img class="h-64 max-w-xs rounded-lg bg-white bg-cover p-6 shadow-lg" src={Weather} alt="Weather App" />
+            <a href="https://actodo-three.vercel.app/" target="_blank" rel="noopener noreferrer">
+              <p class="items-center font-bold p-2 portfolio-highlighter">Visit Website</p>
+            </a>            </div> */}
+          <div class="flex flex-col items-center ">
+            <img class="h-64 max-w-xs rounded-lg bg-white bg-cover p-6 shadow-lg" src={Nostra} alt="Nostra Ecommerce" />
+            <a href="https://monikarajasekaran.github.io/Nosta-Ecommerce/" target="_blank" rel="noopener noreferrer">
+              <p class="items-center font-bold p-2 portfolio-highlighter">Visit Website</p>
+            </a>            </div>
+          {/* <div class="flex flex-col items-center ">
+            <img class="h-64 max-w-xs rounded-lg bg-white bg-cover p-6 shadow-lg" src={Greenden} alt="Greenden" />
+            <a href="https://monikarajasekaran.github.io/Greenden-Tailwindcss/" target="_blank" rel="noopener noreferrer">
+              <p class="items-center font-bold p-2 portfolio-highlighter">Visit Website</p>
+            </a>            </div> */}
+          <div class="flex flex-col items-center ">
+            <img class="h-64 max-w-xs rounded-lg bg-white bg-cover p-6 shadow-lg" src={Triadvisor} alt="Triadvisor" />
+            <a href="https://monikarajasekaran.github.io/Tripadvisor-clone/" target="_blank" rel="noopener noreferrer">
+              <p class="items-center font-bold p-2 portfolio-highlighter">Visit Website</p>
+            </a>            </div>
+          {/* <div class="flex flex-col items-center ">
+            <img class="h-64 max-w-xs rounded-lg bg-white bg-cover p-6 shadow-lg" src={signup} alt="" />
+            <a href="https://actodo-three.vercel.app/" target="_blank" rel="noopener noreferrer">
+              <p class="items-center font-bold p-2 portfolio-highlighter">Visit Website</p>
+            </a>            </div> */}
         </div>
-    
+
       </div>
-      
+
     </section>
   )
 }
